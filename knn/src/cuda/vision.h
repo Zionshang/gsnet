ab0e25171a39b0c7738995ecdba4c43a568abd62
Zishang <zishangji@qq.com>
2025-09-20 13:02:29 +0800

    移除THC依赖

--- knn/src/cuda/vision.h
+++ knn/src/cuda/vision.h
@@ -1,7 +1,8 @@
 #pragma once
 #include <torch/extension.h>
-#include <THC/THC.h>
-
+#include <ATen/ATen.h>
+#include <c10/cuda/CUDAStream.h>
+ 
 void knn_device(float* ref_dev, int ref_width,
     float* query_dev, int query_width,
     int height, int k, float* dist_dev, long* ind_dev, cudaStream_t stream);
--- knn/src/knn.h
+++ knn/src/knn.h
@@ -1,17 +1,13 @@
 #pragma once
 #include "cpu/vision.h"
-
+ 
 #ifdef WITH_CUDA
 #include "cuda/vision.h"
-#include <THC/THC.h>
-extern THCState *state;
+#include <torch/extension.h>
 #endif
-
-
-
+ 
 int knn(at::Tensor& ref, at::Tensor& query, at::Tensor& idx)
 {
-
     // TODO check dimensions
     long batch, ref_nb, query_nb, dim, k;
     batch = ref.size(0);
@@ -19,50 +15,42 @@ int knn(at::Tensor& ref, at::Tensor& query, at::Tensor& idx)
     k = idx.size(1);
     ref_nb = ref.size(2);
     query_nb = query.size(2);
-
-    float *ref_dev = ref.data<float>();
-    float *query_dev = query.data<float>();
-    long *idx_dev = idx.data<long>();
-
-
-
-
-  if (ref.type().is_cuda()) {
+ 
+    float *ref_dev = ref.data_ptr<float>();
+    float *query_dev = query.data_ptr<float>();
+    long *idx_dev = idx.data_ptr<long>();
+ 
+    if (ref.is_cuda()) {
 #ifdef WITH_CUDA
-    // TODO raise error if not compiled with CUDA
-    float *dist_dev = (float*)THCudaMalloc(state, ref_nb * query_nb * sizeof(float));
-
-    for (int b = 0; b < batch; b++)
-    {
-    // knn_device(ref_dev + b * dim * ref_nb, ref_nb, query_dev + b * dim * query_nb, query_nb, dim, k,
-    //   dist_dev, idx_dev + b * k * query_nb, THCState_getCurrentStream(state));
-      knn_device(ref_dev + b * dim * ref_nb, ref_nb, query_dev + b * dim * query_nb, query_nb, dim, k,
-      dist_dev, idx_dev + b * k * query_nb, c10::cuda::getCurrentCUDAStream());
-    }
-    THCudaFree(state, dist_dev);
-    cudaError_t err = cudaGetLastError();
-    if (err != cudaSuccess)
-    {
-        printf("error in knn: %s\n", cudaGetErrorString(err));
-        THError("aborting");
-    }
-    return 1;
+        // TODO raise error if not compiled with CUDA
+        auto dist_dev = at::empty({ref_nb * query_nb}, ref.options().device(at::kCUDA));
+        float *dist_dev_ptr = dist_dev.data_ptr<float>();
+ 
+        for (int b = 0; b < batch; b++) {
+            knn_device(ref_dev + b * dim * ref_nb, ref_nb, query_dev + b * dim * query_nb, query_nb, dim, k,
+                       dist_dev_ptr, idx_dev + b * k * query_nb, c10::cuda::getCurrentCUDAStream());
+        }
+ 
+        cudaError_t err = cudaGetLastError();
+        if (err != cudaSuccess) {
+            printf("error in knn: %s\n", cudaGetErrorString(err));
+            AT_ERROR("aborting");
+        }
+        return 1;
 #else
-    AT_ERROR("Not compiled with GPU support");
+        AT_ERROR("Not compiled with GPU support");
 #endif
-  }
-
-
-    float *dist_dev = (float*)malloc(ref_nb * query_nb * sizeof(float));
-    long *ind_buf = (long*)malloc(ref_nb * sizeof(long));
-    for (int b = 0; b < batch; b++) {
-    knn_cpu(ref_dev + b * dim * ref_nb, ref_nb, query_dev + b * dim * query_nb, query_nb, dim, k,
-      dist_dev, idx_dev + b * k * query_nb, ind_buf);
     }
-
-    free(dist_dev);
-    free(ind_buf);
-
+ 
+    auto dist_dev = at::empty({ref_nb * query_nb}, ref.options().device(at::kCPU));
+    float *dist_dev_ptr = dist_dev.data_ptr<float>();
+    auto ind_buf = at::empty({ref_nb}, ref.options().dtype(at::kLong).device(at::kCPU));
+    long *ind_buf_ptr = ind_buf.data_ptr<long>();
+ 
+    for (int b = 0; b < batch; b++) {
+        knn_cpu(ref_dev + b * dim * ref_nb, ref_nb, query_dev + b * dim * query_nb, query_nb, dim, k,
+                dist_dev_ptr, idx_dev + b * k * query_nb, ind_buf_ptr);
+    }
+ 
     return 1;
-
 }
